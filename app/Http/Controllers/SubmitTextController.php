001d0c8c07d54456f010e9041a3874ad831c6854
DKBdg <strong.every@list.ru>
2022-11-13 23:48:49 +0600

    modified

--- app/Http/Controllers/SubmitTextController.php
+++ app/Http/Controllers/SubmitTextController.php
@@ -7,32 +7,23 @@ use Illuminate\Http\Request;
 
 class SubmitTextController extends Controller
 {
-
+    /*
+    Отправляет полученное текст из формы на сервис messenger
+    */
     public function submitText(Request $request){
-        
         $text = $request->input('userInfo');
         
-        $response = Http::withOptions(['verify'=>false])->post('https://www.google.com/', [
+        $response = Http::withOptions(['verify'=>false])->post('https://localhost::8080/newsletter/', [
             'userText' => $text,
         ]);
-
-        
         if($response->status()===200){
             return redirect('/newsletter')->with('success','Your data was sent successfully');
         }
 
-
         return redirect('/newsletter')->with('error','Error with sending the informaton to the site');
-
-
-    }
-
-    public function index(){
-        return redirect('/newsletter');
     }
 
     public function renderForm(){
-
         return view('newsletter');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubmitTextController;
 
-
-
-Route::get('/', [SubmitTextController::class, 'index']);
-
 Route::get('/newsletter', function () {
     return view('newsletter', [SubmitTextController::class, 'renderForm']);
 });
