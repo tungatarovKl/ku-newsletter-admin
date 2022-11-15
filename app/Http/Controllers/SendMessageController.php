4d0cf54d960782430f3e2089d14910883b8c25f9
Vladimir Kornilov <kornilov@kolesa.kz>
2022-11-15 14:17:15 +0600

    Добавил логирование API запроса к сервису messenger Вынес адрес запроса в .env.example

--- app/Http/Controllers/SendMessageController.php
+++ app/Http/Controllers/SendMessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
  
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SendMessageController extends Controller
 {
@@ -15,18 +16,24 @@ class SendMessageController extends Controller
         $token = env("API_MESSENGER_TOKEN");
 
         try {
-            $response = Http::withOptions(['verify'=>false])->post('http://'.env("API_MESSENGER_HOSTNAME").':8080/newsletter', [
+            $response = Http::withOptions(['verify'=>false])->post(
+                'http://'.env("API_MESSENGER_HOSTNAME").':'.env('API_MESSENGER_PORT').env('API_MESSENGER_URI'),
+            [
                 'message' => $message,
                 'token' => $token
             ]);
         } catch (\Throwable $e) {
+            Log::channel('stderr')->error($e->getMessage());
             return redirect('/newsletter')->with('error','Ошибка соединения с сервером');
         }
 
         if($response->successful()){
             return redirect('/newsletter')->with('success','Сообщение отправлено!');
         }
-
+        Log::channel('stderr')->error([
+            'api_request_url' => $response->effectiveUri(),
+            'api_response_status_code' => $response->status(),
+            'api_response_body' => $response->body()]);
         return redirect('/newsletter')->with('error','Ошибка отправки сообщения');
     }
 
