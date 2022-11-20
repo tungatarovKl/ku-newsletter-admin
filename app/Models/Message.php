898d4aa5c174a94a79b2f99c922845517b65f47a
DKBdg <strong.every@list.ru>
2022-11-20 14:09:28 +0600

    Починил баг с выводом ошибки при использовании лишь одного timestamp

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -9,4 +9,21 @@ class Message extends Model
 {
     use HasFactory;
     protected $table = 'message_history';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->created_at = $model->freshTimestamp();
+        });
+    }
+
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
--- database/migrations/2022_11_19_135031_create_message_history_table.php
+++ database/migrations/2022_11_19_135031_create_message_history_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('message_history', function (Blueprint $table) {
             $table->increments('id');
             $table->text('message_text');
-            $table->timestamps();
+            $table->timestamp('created_at');
         });
     }
 
