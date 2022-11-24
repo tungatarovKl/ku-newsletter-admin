a63e49dfe146a485c06d62b9e4dea490e391fd07
Vladimir Kornilov <kornilov@kolesa.kz>
2022-11-24 14:13:00 +0600

    Обновил bootstrap ссылки. Проверил работоспособность на мобильных устройствах

--- resources/views/base/commonTemplate.blade.php
+++ resources/views/base/commonTemplate.blade.php
@@ -14,7 +14,9 @@
 
 <body>
     @yield('body')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 <footer>
